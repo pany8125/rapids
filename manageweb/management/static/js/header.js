6d1438603150b8d4716fd87a5a156776f40e2734
scott.xie <scott.xie@runmit.com>
2017-03-09 22:22:16 +0800

    add management pages

---
+++ manageweb/management/static/js/header.js
@@ -0,0 +1,110 @@
+var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+var decode64 = function(input) {
+	if(!isValid(input)){
+		Ext.MessageBox.alert('系统提示',"cookie超时,请重新登陆系统",function(){
+			top.location.href='/';
+		});
+		return;
+	}
+	var output = "";
+	var chr1, chr2, chr3 = "";
+	var enc1, enc2, enc3, enc4 = "";
+	var i = 0;
+
+	var base64test = /[^A-Za-z0-9\+\/\=]/g;
+	if (base64test.exec(input)) {
+		Ext.Msg
+				.alert("Warning",
+						"The document contains invalid characters. Errors can occur when decoding it.");
+	}
+	input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+	do {
+		enc1 = keyStr.indexOf(input.charAt(i++));
+		enc2 = keyStr.indexOf(input.charAt(i++));
+		enc3 = keyStr.indexOf(input.charAt(i++));
+		enc4 = keyStr.indexOf(input.charAt(i++));
+
+		chr1 = (enc1 << 2) | (enc2 >> 4);
+		chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+		chr3 = ((enc3 & 3) << 6) | enc4;
+
+		output = output + String.fromCharCode(chr1);
+
+		if (enc3 != 64) {
+			output = output + String.fromCharCode(chr2);
+		}
+		if (enc4 != 64) {
+			output = output + String.fromCharCode(chr3);
+		}
+
+		chr1 = chr2 = chr3 = "";
+		enc1 = enc2 = enc3 = enc4 = "";
+	} while (i < input.length);
+
+	return unescape(output);
+};
+
+var path = "/clotho-management", adminID = decode64(Ext.util.Cookies
+		.get("adminId")), adminName = decode64(Ext.util.Cookies
+		.get("adminName"));
+var task = new Ext.util.DelayedTask(function() {
+	location.href = '/management/login.html';
+});
+
+Ext.onReady(function() {
+	if (!adminID) {
+		Ext.Msg.alert('系统提示', '请重新登陆');
+		task.delay(2000);
+	}
+});
+
+function checkCommonImgType(val){
+	var strRegex = '.jpg|.jpeg|.gif|.bmp|.png|';
+	var fileExt=val.substr(val.lastIndexOf(".")).toLowerCase();
+	if(strRegex.indexOf(fileExt+"|")!=-1)//如果图片文件，则进行图片信息处理
+	{
+		return true;
+	}else{
+		Ext.Msg.alert('系统提示', '仅支持jpg,jpeg,gif,bmp,png');
+		return false;
+	}
+}
+
+function isValid(val){
+	if(typeof(val) == "undefined" || val==null || val=='' || val==0){
+		return false;
+	}else{
+		return true;
+	}
+}
+
+Ext
+		.apply(
+				Ext.form.VTypes,
+				{
+					url : function(val, field) {
+						try {
+							var patrn=/^((http|https):\/\/)/i; 
+							if (patrn.exec(val))
+								return true;
+							else
+								return false;
+						} catch (e) {
+							return false;
+						}
+					},
+					urlText : '请输入有效的URL地址.',
+					num_character : function(val, field) {
+						try {
+							var strRegex = /^[0-9a-zA-Z]*$/g;
+							if (strRegex.test(val))
+								return true;
+							return false;
+						} catch (e) {
+							return false;
+						}
+					},
+					num_characterText : '请输入数字或字母.'
+				});
---
+++ manageweb/management/static/js/login.js
@@ -0,0 +1,183 @@
+var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+var encode64 = function(input) {
+    input = escape(input);
+    var output = "";
+    var chr1, chr2, chr3 = "";
+    var enc1, enc2, enc3, enc4 = "";
+    var i = 0;
+    
+    do {
+        chr1 = input.charCodeAt(i++);
+        chr2 = input.charCodeAt(i++);
+        chr3 = input.charCodeAt(i++);
+        
+        enc1 = chr1 >> 2;
+        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+        enc4 = chr3 & 63;
+        
+        if (isNaN(chr2)) {
+            enc3 = enc4 = 64;
+        } else if (isNaN(chr3)) {
+            enc4 = 64;
+        }
+        
+        output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4);
+        chr1 = chr2 = chr3 = "";
+        enc1 = enc2 = enc3 = enc4 = "";
+    } while (i < input.length);
+    
+    return output;
+}
+
+var path="/clotho-management";
+
+Ext.onReady(function() {
+	//Ext.QuickTips.init();
+	
+	var userName = new Ext.form.TextField({
+		name : "name",
+		id : "name",
+		fieldLabel : "用户名",
+		blankText : "请输入用户名",
+		allowBlank: false,
+		selectOnFocus : true,
+		cls: 'login_user',
+		anchor: '95%',
+		value:''
+	});
+	
+	var password = new Ext.form.TextField({
+		cls : "key",
+		name : "password",
+		id : "password",
+		fieldLabel : "密&nbsp;&nbsp;&nbsp;码",
+		blankText : "请输入密码",
+		allowBlank: false,
+		selectOnFocus : true,
+		inputType : "password",
+		cls: 'login_password',
+		anchor: '95%',
+		value:""
+	});
+	
+	/*var randCode = new Ext.form.TextField({
+		cls : "key",
+		name : "randCode",
+		id:'randCode', 
+		fieldLabel : "验证码",
+		blankText : "验证码不能为空",
+		width:80, 
+		allowBlank: false,
+		selectOnFocus : true
+	});*/
+	
+	var form = new Ext.FormPanel({
+		region: 'center',
+		labelAlign : "right",
+		labelWidth : 55,
+		labelPad : 0,
+		frame : true,
+		//items : [{html:'&nbsp'},userName, password,randCode,{html:'&nbsp'}],
+		items : [{html:'&nbsp'},userName, password,{html:'&nbsp'}],
+		listeners: {
+			afterlayout : function(){
+				password.focus();
+			}
+		
+		}
+	});
+	
+	userName.on("specialkey", function(f, e) {
+		if (e.getKey() == e.ENTER) {
+			login();
+		}
+	}, this);
+	
+	password.on("specialkey", function(f, e) {
+		if (e.getKey() == e.ENTER) {
+			login();
+		}
+	}, this);
+	
+	var win = new Ext.Window({
+		el : "win",
+		
+		title : "重构记忆后台管理系统",
+		width : 300,
+		height : 180,
+		layout: 'border',
+		resizable : false,
+		plain : true,
+//		autoScroll : true,
+		closable : false,
+		border: false,
+		buttons : [{
+			text : "登录",
+			id : "login",
+			minWidth: 50,
+			handler : login
+		}, {
+			text: "重置",
+			minWidth: 50,
+			handler : function() {
+				form.getForm().reset();
+			}
+		}],
+		items : [form]
+	});
+
+	win.show();
+	/*var bd = Ext.getDom('randCode'); 
+	var bd2 = Ext.get(bd.parentNode); 
+	 bd2.createChild([{
+		tag: 'span',
+		html: ' <a href="javascript:reloadcode();">'
+		}, {
+		tag: 'img',
+		id: 'safecode',
+		src: 'image.jsp',
+		align: 'absbottom'
+		}, {
+		tag: 'span',
+		html: '</a>'
+		}]);*/
+	function login(){
+
+		if (!form.getForm().isValid()) {
+			Ext.Msg.alert("提示","请输入用户名和密码",function(){
+				userName.focus();
+			});
+			return;
+		}
+		
+        form.getForm().submit({
+			waitTitle : '系统信息',
+			url: path + '/login/loginValid',   
+ 			method : 'post',   //指定发送方式
+ 			waitMsg:"正在验证登录用户,请稍候..."  ,
+			clientValidation: true,
+            success:function(form, action){ 
+				var admin = eval('('+action.result.msg+')');
+				var d = new Date();
+				d.setTime((new Date()).getTime()+60*60*1000);//cookie expires:1h
+				Ext.util.Cookies.set('adminName',encode64(admin.name),d);
+				Ext.util.Cookies.set('adminId',encode64(admin.id),d);
+        		if(window.parent){
+        			window.parent.location.href = 'views/index.html';
+        		}else{
+        			window.location.href = path + 'views/index.html';
+        		}
+				
+            },
+            failure: function(form, action) {
+            	if(!action.result){
+            		Ext.Msg.alert("提示","网络错误,请重新登录");  
+            	}else {
+            		Ext.Msg.alert("提示",action.result.msg);  
+            	}
+		    }
+    	});  
+	}
+});
